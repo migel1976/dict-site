3810556cb2d967712a671288df3a1cb4b620c626
МС <ms@MacBook-Pro-MS.local>
2020-04-30 12:11:33 +0300

    Updates

---
+++ precache-manifest.7d5091a4cf66b634beee2025966e4b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ad2ec7b956763d602882f5a19a5d8a",
+    "url": "/dict-site/index.html"
+  },
+  {
+    "revision": "81e83fea705714141efc",
+    "url": "/dict-site/static/css/main.4d111919.chunk.css"
+  },
+  {
+    "revision": "53cdfe5431217519e3d9",
+    "url": "/dict-site/static/js/2.16e73ba9.chunk.js"
+  },
+  {
+    "revision": "d14f21e42c09509e44bd25febcf4b2dc",
+    "url": "/dict-site/static/js/2.16e73ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e83fea705714141efc",
+    "url": "/dict-site/static/js/main.c1e5fb97.chunk.js"
+  },
+  {
+    "revision": "4d9c6fcfdbd65b8cc3c1",
+    "url": "/dict-site/static/js/runtime-main.fc23049d.js"
+  },
+  {
+    "revision": "fc5e74481b00f9afbcf1d3913cc007e4",
+    "url": "/dict-site/static/media/paper.fc5e7448.jpg"
+  }
+]);
